866a81711f1bf766e10f370e19c2a5ebba902b6f
DarioDa86 <dario.dagnelli@gmail.com>
2021-11-08 18:57:01 +0100

    bonus completato

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Best DB MOvies</title>
+    <link rel="stylesheet" href="css/app.css">
+
 </head>
 <body>
+    <header>
+        <div class="block-logo">
+            <img src="{{asset("images/BlockBuster-logo.png")}}" alt="">
+        </div>
+    </header>
     
-    <ul>
+    <ul class="movies-container">
         @foreach ($movies as $movie)
-        <h3>Titolo: <span class="risultato">{{$movie["title"]}}</span></h3>
-        <h3>Titolo originale: {{$movie["original_title"]}}</h3>
-        <h3>Nazionalità: {{$movie["nationality"]}}</h3>
+        <li class="card">
+            <h3>Titolo: <span class="risultato">{{$movie["title"]}}</span></h3>
+            <h4>Titolo originale: <span class="risultato">{{$movie["original_title"]}}</span></h4>
+            <h4>Nazionalità: <span class="risultato">{{$movie["nationality"]}}</span></h4>
+            <h4>Data di uscita: <span class="risultato">{{$movie["date"]}}</span></h4>
+            <h4>Voto: <span class="risultato">{{$movie["vote"]}}</span></h4>
+        </li>
         @endforeach
     </ul>
 </body>
